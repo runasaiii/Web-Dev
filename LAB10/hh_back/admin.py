2a04045514a8c9169cfd93656b6f95ae38806c18
Aruna <138991935+runasaiii@users.noreply.github.com>
2025-04-13 16:04:37 +0500

    lab10

--- LAB10/hh_back/admin.py
+++ LAB10/hh_back/admin.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from .models import Company, Vacancy
 
-# Регистрация модели Company в админке
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'city', 'address')  # Поля для отображения в списке
-    search_fields = ('name', 'city')  # Поля для поиска
-    list_filter = ('city',)  # Фильтр по городу
+    list_display = ('name', 'city', 'address') 
+    search_fields = ('name', 'city')  
+    list_filter = ('city',) 
 
-# Регистрация модели Vacancy в админке
 class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'salary', 'company')  # Поля для отображения в списке
+    list_display = ('name', 'salary', 'company')
     search_fields = ('name',)  # Поля для поиска
-    list_filter = ('salary', 'company')  # Фильтр по зарплате и компании
+    list_filter = ('salary', 'company')  
 
-# Регистрируем модели с их настройками
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Vacancy, VacancyAdmin)
